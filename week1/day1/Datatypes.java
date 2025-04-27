93a254c7b8d54e916c79f576a9adc9d271bc4d31
kesavan-120 <kesavan848183@gmail.com>
2025-04-27 09:24:11 +0530

    Add files via upload
    Day1(26-04-2025) Assignments

---
+++ week1/day1/Datatypes.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		int code=125646446;
+		byte age=15;
+		long phno= 9941948564L;
+		short regno= 12345;
+		float speed= 3.15645554455544f;
+		double pace= 6.12154654454545;
+		char logo = 'D';
+		boolean isDisplayVisible = true;
+		String coachname= "Nellaiexpress";
+		
+		System.out.println(code);
+		System.out.println(age);
+		System.out.println(phno);
+		System.out.println(regno);
+		System.out.println(speed);
+		System.out.println(pace);
+		System.out.println(logo);
+		System.out.println(isDisplayVisible);
+		System.out.println(coachname);
+		
+		
+	}
+
+}
