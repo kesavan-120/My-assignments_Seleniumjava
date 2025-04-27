a22ac8ebdcb4b9a720aea358e73da10fec38af8d
kesavan-120 <kesavan848183@gmail.com>
2025-04-27 21:32:09 +0530

    Add files via upload
    Week1Day2 Assignments

---
+++ week1day2/Oddnumbers.java
@@ -0,0 +1,17 @@
+package week1day2;
+
+public class Oddnumbers {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 20; i++) {
+			// i is value % is multiple of 2 != is reminder should be 1 not 0
+			if (i % 2 != 0) {
+				System.out.println("Odd numbers" + i);
+
+			}
+
+		}
+
+	}
+
+}
